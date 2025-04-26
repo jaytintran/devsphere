1283c7a40821a44420d71347469c0280f8f5cfb9
Tin <jaytintran@gmail.com>
2025-04-26 12:11:13 +0200

    init: tailwind setup + font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
+const poppins = Poppins({
 	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"], // Pick the weights you need
+	variable: "--font-poppins", // You can name your CSS variable
 });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
+const spaceGrotesk = Space_Grotesk({
 	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	variable: "--font-space-grotesk",
 });
 
 export const metadata: Metadata = {
@@ -23,7 +26,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" className={`${poppins.variable} ${spaceGrotesk.variable}`}>
 			<body>{children}</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 const Home = () => {
 	return (
 		<main>
-			<h1 className="text-3xl text-violet-200 font-black">DevSphere</h1>
+			<h1 className="text-3xl text-violet-200 font-black font-space-grotesk">
+				DevSphere
+			</h1>
+			<h1 className="text-3xl text-violet-200 font-normal font-poppins">
+				DevSphere
+			</h1>
 		</main>
 	);
 };
